412ab414c85bb19086503cb975aaa79d27e5f539
xkdr1234 <15055905110@qq.com>
2021-03-06 16:15:59 +0800

    优化代码，同时删除了启动时的一大堆东西

--- console.py
+++ console.py
@@ -3,84 +3,14 @@
 import os
 import time
 import datetime
-print("""
-                                  _oo0oo_
-                                 088888880
-                                 88" . "88
-                                 (| -_- |)
-                                  0\ = /0
-                               ___/'---'\___
-                             .' \\\\|     |// '.
-                            / \\\\|||  :  |||// \\
-                           /_ ||||| -:- |||||- \\
-                          |   | \\\\\\  -  /// |   |
-                          | \_|  ''\---/''  |_/ |
-                          \  .-\__  '-'  __/-.  /
-                        ___'. .'  /--.--\  '. .'___
-                     ."" '<  '.___\_<|>_/___.' >'  "".
-                    | | : '-  \'.;'\ _ /';.'/ - ' : | |
-                    \  \ '_.   \_ __\ /__ _/   .-' /  /
-                ====='-.____'.___ \_____/___.-'____.-'=====
-                                  '=---='
-  
-  
-              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                        佛祖保佑    iii    永不死机
-                                  永无BUG
-""")
-time.sleep(1.2)
-print("""
-▄▄▄▄▄▄▄▄  ▄▄▄▄                  ▄▄▄▄      ▄▄▄▄
- ██▀▀▀▀▀▀  ▀▀██                 ██▀▀██   ▄█▀▀▀▀█
- ██          ██      ▀██  ███  ██    ██  ██▄
- ███████     ██       ██▄ ██   ██    ██   ▀████▄
- ██          ██        ████▀   ██    ██       ▀██
- ██          ██▄▄▄      ███     ██▄▄██   █▄▄▄▄▄█▀
- ▀▀           ▀▀▀▀      ██       ▀▀▀▀     ▀▀▀▀▀
-                      ███
-""")
-print("By:FlyOS Boot")
-time.sleep(1)
-print("FlyOS Console")
-#Starting Services
-os.system("sh $FLYOS/services.sh")
-#info print
-os.system("termimage $FLYOS/img/icon.png")
-print("您的下一台电脑，不是电脑，而是手机！-----FlyOS新理念")
-print("_________________________________")
-os.system("termimage $FLYOS/img/welcome.jpg")
-print("欢迎使用！")
-print("___________")
-print("系统检查")
-print("___________")
-print("CPU信息")
-os.system("lscpu")
 
-print("CPU负载")
-os.system("uptime")
-
-print("内核信息:")
-os.system("uname -a")
-
-print("磁盘信息:")
-os.system("df -h")
-
-print("内存信息:")
-os.system("free -m")
-
-print("网络信息:wlan0")
-os.system("ifconfig wlan0")
-
-print("系统信息:")
-os.system("screenfetch")
-#注册读取
 n = open('/data/data/com.termux/files/usr/etc/flyos/database/name.db','r')
 name = n.read()
 print("_____ _        ___  ____")
-print("|  ___| |_   _ / _ \/ ___|")
-print("| |_  | | | | | | | \___ \.")
+print("|  ___| |_   _ / _ \\/ ___|")
+print("| |_  | | | | | | | \\___ \\.")
 print("|  _| | | |_| | |_| |___) |")
-print("|_|   |_|\__, |\___/|____/")
+print("|_|   |_|\\__, |\\___/|____/")
 print("         |___/")
 print("_________________________")
 print("__________FlyOS__________")
@@ -120,11 +50,19 @@ print("21.初始化FlyOS")
 print("22.启动Xfce4图形化(端口5902)")
 print("0.进入终端")
 print("如需再次打开FlyOS Console，进入终端输入flyos即可")
-print("####FlyOS Panel已经启动，浏览器访问http://IP:8888，WebShell请浏览器访问http://IP:4200，WEB虚拟机请访问http://IP:8002，Apache服务请访问http://IP:8080，Nginx在http://IP:8088，HTTP文件管理器在http://IP:8081，FlyOS AM调用在http://IP:5000，Termux:API调用在http://IP:5002。注意，本地访问请浏览器访问http://127.0.0.1:端口号####")
+print("####FlyOS Panel已经启动，"
+        "浏览器访问http://IP:8888，"
+        "WebShell请浏览器访问http://IP:4200，"
+        "WEB虚拟机请访问http://IP:8002，"
+        "Apache服务请访问http://IP:8080，"
+        "Nginx在http://IP:8088，"
+        "HTTP文件管理器在http://IP:8081，"
+        "FlyOS AM调用在http://IP:5000，"
+        "Termux:API调用在http://IP:5002。"
+        "注意，本地访问请浏览器访问http://127.0.0.1:端口号####")
 while 1:
     num = input("请输入要启动的编号，例如:1 :")
     print("正在启动项目" + num)
-    
     if num == '1':
         os.system("python3 $FLYOS/deploylinux/deploy.py")
     elif num == '2':
@@ -171,7 +109,7 @@ while 1:
     elif num == '21':
         os.system("python $FLYOS/.firstuse/register.py")
     elif num == '22':
-        print("Xfce4图形化界面启动在IP:5902，本地连接请输入IP:127.0.0.1:5902") 
+        print("Xfce4图形化界面启动在IP:5902，本地连接请输入IP:127.0.0.1:5902")
         os.system("nohup flyosvnc &")
     elif num == '0':
         os.system('~/.termux/shell')
