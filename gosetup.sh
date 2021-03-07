4f2b7aa97ad82c42b8304a3a241c157b188905fb
xkdr1234 <50104849+xkdr1234@users.noreply.github.com>
2021-03-07 15:44:10 +0800

    Update gosetup.sh

--- gosetup.sh
+++ gosetup.sh
@@ -1,3 +1,9 @@
-wget http://d.ixcmstudio.cn:21188/doc/FlyOS/utsetup.sh
-chmod 777 utsetup.sh
-sh utsetup.sh
+echo "安装将会清除所有数据, 请确认已备份重要数据"
+read -p "开始安装 [Y/n] " c
+if [ `echo $c | tr [a-z] [A-Z]` == "Y" | $c == "" ]; then
+echo "[*] 下载文件"
+wget "https://xingyujie-my.sharepoint.com/:u:/g/personal/xingyujie_xingyujie_onmicrosoft_com/EeURQe0wCG1PheBdnX6PanoBRw7hlVhcBcXQqu3UWRRw2w?e=fFMUcx&download=1" -O $PREFIX/tmp/flyos.tar.gz >/dev/null 2>&1
+echo "[*] 解压文件"
+cd $PREFIX/../
+tar -zxvf $PREFIX/tmp/flyos_3.3.tar.gz --recursive-unlink --preserve-permissions
+echo "[+] 安装完成, 请重启termux"
