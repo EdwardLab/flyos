0cc6f734238c59ee9c8b0f9e93f559543c3b7f91
xkdr1234 <15055905110@qq.com>
2021-03-09 13:08:29 +0800

    Update gosetup.sh

--- gosetup.sh
+++ gosetup.sh
@@ -4,7 +4,7 @@ read -p "按下回车继续" _
 echo "[*] 安装依赖"
 pkg install wget -y
 echo "[*] 下载文件"
-wget --show-progress -q "https://xingyujie-my.sharepoint.com/:u:/g/personal/xingyujie_xingyujie_onmicrosoft_com/EeURQe0wCG1PheBdnX6PanoBRw7hlVhcBcXQqu3UWRRw2w?e=fFMUcx&download=1" -O $PREFIX/tmp/flyos.tar.gz
+wget --show-progress -q "https://xingyujie-my.sharepoint.com/:u:/g/personal/xingyujie_xingyujie_onmicrosoft_com/Ecwrn2nsOUVBomkKQcX5O4MBH3J7aAcbvwXgE1V_GVqr_A?e=N0pfFJ&download=1" -O $PREFIX/tmp/flyos.tar.gz
 echo "[*] 解压文件"
 cd $PREFIX/../
 tar -zxvf $PREFIX/tmp/flyos.tar.gz --recursive-unlink --preserve-permissions
