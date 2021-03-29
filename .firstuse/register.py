c6d76bc43528cae51a192b835f7b1f91b0211a81
xkdr1234 <15055905110@qq.com>
2021-03-29 23:24:25 +0800

    提升与旧版本兼容性

--- .firstuse/register.py
+++ .firstuse/register.py
@@ -13,6 +13,7 @@ import termux_auth
 
 FLYOS_ROOT = os.getenv('FLYOS')
 HOME = os.getenv("HOME")
+
 #Tips
 print("______________________________________")
 print("欢迎使用FlyOS!")
@@ -20,17 +21,18 @@ print("请使用本机浏览器访问:http://127.0.0.1:5005 来初始化FlyOS")
 #FlyOS 激活main
 def main():
     """主方法"""
-    set_env(title="欢迎使用FlyOS", auto_scroll_bottom=True)
-    put_html("<h1>欢迎使用！ -- FlyOS 初始化向导</h1>")
-    put_text('欢迎使用FlyOS!开始初始化您的FlyOS吧！')
-    popup('Hi,There！欢迎使用FlyOS！让我们来初始化FlyOS吧！')
-    password = pywebio.input.input("设置您的系统开机密码:", type="password")
-    vpwd = pywebio.input.input("请再次输入密码验证:", type="password")
-    if password != vpwd:
-        popup('错误', "两次输入密码不一致，请刷新页面重试")
-        return
-    put_text('初始化完成！请进入termuxAPP开始体验FlyOS吧！')
-    termux_auth.change_passwd(password)
+    if not os.path.exists(FLSOS_ROOT+"/.firstuse/lock"):
+        set_env(title="欢迎使用FlyOS", auto_scroll_bottom=True)
+        put_html("<h1>欢迎使用！ -- FlyOS 初始化向导</h1>")
+        put_text('欢迎使用FlyOS!开始初始化您的FlyOS吧！')
+        popup('Hi,There！欢迎使用FlyOS！让我们来初始化FlyOS吧！')
+        password = pywebio.input.input("设置您的系统开机密码:", type="password")
+        vpwd = pywebio.input.input("请再次输入密码验证:", type="password")
+        if password != vpwd:
+            popup('错误', "两次输入密码不一致，请刷新页面重试")
+            return
+        put_text('初始化完成！请进入termuxAPP开始体验FlyOS吧！')
+        termux_auth.change_passwd(password)
     try:
         os.mkdir(f"{HOME}/.flyos")
     except FileExistsError:
