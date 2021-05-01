89c127e0aee9f854692dda0abb637a510185e763
xkdr1234 <15055905110@qq.com>
2021-05-01 10:17:19 +0800

    Update gosetup.sh

--- gosetup.sh
+++ gosetup.sh
@@ -2,11 +2,15 @@
 echo "安装将会清除所有数据, 请确认已备份重要数据"
 read -p "按下回车继续" _
 echo "[*] 安装依赖"
-pkg install wget -y
+pkg install wget git -y
 echo "[*] 下载文件"
 rm -rf $PREFIX/tmp/flyos.tar.gz
-wget --show-progress -q "https://xingyujie-my.sharepoint.com/:u:/g/personal/xingyujie_xingyujie_onmicrosoft_com/EfqCU1iWEBtBloitHDxCTKcBYiMJ9bEiDVeAzEi6wPLSVA?e=2bwcBl&download=1" -O $PREFIX/tmp/flyos.tar.gz
+wget --show-progress -q "https://xingyujie-my.sharepoint.com/:u:/g/personal/xingyujie_xingyujie_onmicrosoft_com/EfqCU1iWEBtBloitHDxCTKcBYiMJ9bEiDVeAzEi6wPLSVA?e=ZdnmMV&download=1" -O $PREFIX/tmp/flyos.tar.gz
 echo "[*] 解压文件"
 cd $PREFIX/../
 tar -zxvf $PREFIX/tmp/flyos.tar.gz --recursive-unlink --preserve-permissions
+echo "[*] 更新系统"
+cd $PREFIX/etc/flyos
+git pull
+pip install -r requirements.txt
 echo "[+] 安装完成, 请重启termux"
