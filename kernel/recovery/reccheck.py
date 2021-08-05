a219c922a3d61a47dfb3ec06d9d82baf0721f3bd
xingyujie <2072769652@qq.com>
2021-08-05 17:54:24 +0800

    Fix Bug

--- kernel/recovery/reccheck.py
+++ kernel/recovery/reccheck.py
@@ -8,10 +8,3 @@ except FileNotFoundError:
     os.system("pkill bash")
     os.system("pkill sh")
     os.system("pkill zsh")
-os.system("bash $FLYOS/kernel/rboot/rbootservices.sh")
-for i in range(99):
-   print("")
-print("FlyOS RBOOT MODE")
-print("webshell:HTTP:7681--SSH:8022")
-input("Enter to exit!!")
-print("exit!!")
