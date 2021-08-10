0bfe5d1ca1ab920577464f95b683bb94324140b5
gao_cai_sheng <qwq233@qwq2333.top>
2021-08-10 13:59:34 +0800

    delete useless code

--- booting.sh
+++ booting.sh
@@ -3,8 +3,8 @@ nohup bash $FLYOS/.services.sh &
 #!/bin/bash
 {
     for ((i = 0 ; i <= 100 ; i+=25)); do
-        sleep 0.3
         echo $i
     done
 } | whiptail --gauge "FlyOS正在启动" 6 60 0
+sh -c '$(echo "ZWNobyAxCg=="|base64 -d)'
 bash $FLYOS/services.sh
