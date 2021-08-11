03050f04d231e83b0ac0637ac8146d505f603105
WhaleFall <2734184475@qq.com>
2021-08-11 11:44:59 +0800

    更新 server.py

--- panel/server.py
+++ panel/server.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 # 飞屎OS-bate Panel By:请遵守开源协议
 # Use Under License GPL - V3
+=======
+#FlyOS Panel By:XingYuJie
+#Use Under License GPL - V3
+>>>>>>> parent of 26fff1b (🌈 style(web首页): 更改版权部分)
 import socket
 
 import pywebio.input
@@ -10,22 +15,26 @@ from pywebio.session import set_env
 print("______________________________________")
 print("飞屎OS-bate Panel")
 print("启动中")
+<<<<<<< HEAD
 # 飞屎OS-bate WEB Panel main
 
 
+=======
+#FlyOS WEB Panel main
+>>>>>>> parent of 26fff1b (🌈 style(web首页): 更改版权部分)
 def get_host_ip():
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
+        s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8',80))
+        ip=s.getsockname()[0]
     finally:
         s.close()
     return ip
 
-
 def main():
     set_env(title="飞屎OS-bate Panel",
             auto_scroll_bottom=True
+<<<<<<< HEAD
             )
     put_html("<h1>飞屎OS-bate WEB Panel</h1>")
     put_text('飞屎OS-bate Panel By:请遵守开源协议',
@@ -37,46 +46,60 @@ def main():
           '请务必提交到邮箱:110github@gmail.com谢谢！'
           '程序由Python-pywebio强力驱动'
           )
+=======
+        )
+    put_html("<h1>FlyOS WEB Panel</h1>")
+    put_text('FlyOS Panel By:XingYuJie',
+            sep=' '
+        )
+    popup('欢迎使用FlyOS Panel！',
+            '欢迎使用FlyOS WEB Panel！'
+            '如果程序有Bug，'
+            '请务必提交到邮箱:xingyujie50@gmail.com谢谢！'
+            '程序由MicroTech Projects -- FlyOS强力驱动'
+        )
+>>>>>>> parent of 26fff1b (🌈 style(web首页): 更改版权部分)
     put_link("web终端",
-             url=f'http://{get_host_ip()}:7681'
-             )
+            url=f'http://{get_host_ip()}:7681'
+        )
     put_text('_______________________',
-             sep=' '
-             )
+            sep=' '
+        )
     put_link("VM虚拟机",
-             url=f'http://{get_host_ip()}:8002'
-             )
+            url=f'http://{get_host_ip()}:8002'
+        )
     put_text('_______________________',
-             sep=' '
-             )
+            sep=' '
+        )
     put_link("vscode",
-             url=f'http://{get_host_ip()}:2001'
-             )
+            url=f'http://{get_host_ip()}:2001'
+        )
     put_text('_______________________',
-             sep=' '
-             )
+            sep=' '
+        )
     put_link("Apache主页",
-             url=f'http://{get_host_ip()}:8080'
-             )
+            url=f'http://{get_host_ip()}:8080'
+        )
     put_text('_______________________',
-             sep=' '
-             )
+            sep=' '
+        )
     put_link("Nginx主页",
-             url=f'http://{get_host_ip()}:8088'
-             )
+            url=f'http://{get_host_ip()}:8088'
+        )
     put_text('_______________________',
-             sep=' '
-             )
+            sep=' '
+        )
     put_link("HTTP文件服务器",
-             url=f'http://{get_host_ip()}:8081'
-             )
+            url=f'http://{get_host_ip()}:8081'
+        )
     put_text('_______________________',
-             sep=' '
-             )
+            sep=' '
+        )
     put_link("jupyter notebook",
-             url=f'http://{get_host_ip()}:2000'
-             )
+            url=f'http://{get_host_ip()}:2000'
+        )
     put_text('_______________________',
+<<<<<<< HEAD
              sep=' '
              )
     put_link("飞屎OS-bate桌面环境",
@@ -95,6 +118,25 @@ def main():
              url=f'http://{get_host_ip()}:5002'
              )
 
+=======
+            sep=' '
+        )
+    put_link("FlyOS桌面环境",
+            url=f'http://{get_host_ip()}:6081/vnc.html'
+        )
+    put_text('_________系统工具__________',
+            sep=' '
+        )
+    put_link("FlyOS AM调用 ",
+            url=f'http://{get_host_ip()}:5000'
+        )
+    put_text('_______________________',
+            sep=' '
+        )
+    put_link("FlyOS Termux:API调用 ",
+            url=f'http://{get_host_ip()}:5002'
+            )
+>>>>>>> parent of 26fff1b (🌈 style(web首页): 更改版权部分)
 
 if __name__ == '__main__':
     # print(get_host_ip())
