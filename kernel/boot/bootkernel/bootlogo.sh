ef77d374d6b55ed08ed5d8d8c704d5a2ce4933b6
WhaleFall <2734184475@qq.com>
2021-08-12 12:08:49 +0800

    🐞 fix: 删除不必要的屎核,,进一步稳定屎山

--- kernel/boot/bootkernel/bootlogo.sh
+++
@@ -1,8 +0,0 @@
-
-clear
-termimage $飞屎OS-bate/kernel/boot/bootkernel/logo.png 
-sleep 2
-echo 加载飞屎OS-bate内核配置与系统检查
-##RecoveryMode请去掉注释，下次将会启动到Recovery
-
-bash $飞屎OS-bate/kernel/boot/kernelconfig/kernelconf.sh
