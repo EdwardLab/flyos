3cefd7f7e3b0c12c633d3691cee3bac705a90941
Edward Hsing <xingyujie50@gmail.com>
2024-06-22 17:22:36 +0800

    updated config

--- dashboard/start.py
+++ dashboard/start.py
@@ -1,10 +1,15 @@
 import os
 from config import *
+from files.userspace.cli import *
+from tools import *
+from files.userspace.cli import *
+import subprocess
 #basic
 os.system("""
 adb start-server
 """)
 os.system('hostname ' + hostname)
+
 #clean
 def logs_check():
     files = [
@@ -39,27 +44,39 @@ if boot_dashboard:
             os.system(f"""
             nohup gunicorn -b {dashboard_host_addr}:{server_port} --chdir /flyos main:app >> /flyos/logs/flyos_main.log 2>&1 &
             """)
-
+            
+if boot_userspace:
+    userspace_start()
+if userspace_ttyd:
+    os.system(f'nohup ttyd -p {userspace_ttyd_port} --writable userspace_login >> /flyos/logs/ttyd_userspace.log 2>&1 &')
+    
 # SSH and shell service
+
 if boot_ssh:
     os.system(f"""
-    nohup ttyd -p {terminal_port} login >> /flyos/logs/ttyd.log 2>&1 &
-    nohup ttyd -p {android_terminal_port} android_shell >> /flyos/logs/ttyd_android.log 2>&1 &
+    nohup ttyd -p {terminal_port} --writable login >> /flyos/logs/ttyd.log 2>&1 &
+    nohup ttyd -p {android_terminal_port} --writable android_shell >> /flyos/logs/ttyd_android.log 2>&1 &
     nohup /etc/init.d/ssh start >> /flyos/logs/ssh.log 2>&1 &
+    """)
+if boot_userspace_ssh:
+    exec_userspace(f"""
+nohup /etc/init.d/ssh start >> /flyos/logs/userspace_sshd.log 2>&1 &
     """)
 # VNC service
 if boot_default_vnc:
-    os.system(f"""
-    rm -rf /tmp/.X{vnc_default_port}-lock
-    rm -rf /tmp/.X11-unix/X{vnc_default_port}
-    nohup vncserver :{vnc_default_port} -geometry {vnc_default_geometry} -localhost {vnc_default_localhost} >> /flyos/logs/vnc_default.log 2>&1 &
+    user = userspace_vnc_login_user
+    exec_userspace(f"""
+rm -rf /tmp/.X11-unix/X*
+rm -rf /tmp/.X*-lock
+su - {user} -c 'nohup vncserver :{vnc_default_port} -geometry {vnc_default_geometry} -localhost {vnc_default_localhost} >> /flyos/logs/vnc_default.log 2>&1 &'
     """)
+
 if boot_vnc_1920x1080:
-    os.system("""
-    rm -rf /tmp/.X2-lock
-    rm -rf /tmp/.X11-unix/X2
-    nohup vncserver :2 -geometry 1920x1080 -localhost no >> /flyos/logs/vnc_1920x1080.log 2>&1 &
+    user = userspace_vnc_login_user
+    exec_userspace(f"""
+su - {user} -c 'nohup vncserver :2 -geometry 1920x1080 -localhost {vnc_default_localhost} >> /flyos/logs/vnc_1920x1080.log 2>&1 &'    
     """)
+
 if boot_vnc:
     os.system(f"""
     nohup /flyosext/novnc/utils/novnc_proxy --vnc localhost:590{vnc_default_port} --listen {novnc_proxy_addr}:{vnc_port} >> /flyos/logs/novnc.log 2>&1 &
@@ -68,11 +85,6 @@ if boot_vnc:
 if boot_code_server:
     os.system("""
     nohup code-server >> /flyos/logs/code_server.log 2>&1 &
-    """)
-
-if boot_jupyter:
-    os.system(f"""
-    nohup python3 -m jupyter notebook --no-browser --allow-root --ip={jupyter_host_addr} --port={jupyter_notebook_port} --notebook-dir=/ >> /flyos/logs/jupyter.log 2>&1 &
     """)
 
 if boot_file_browser:
--- dashboard/sysconf.py
+++ dashboard/sysconf.py
@@ -1,5 +1,5 @@
-os_ver = "3.12"
-os_build_channel = "alpha"
-cust_build = "build3012_preview"
+os_ver = "3.2"
+os_build_channel = "3200"
+cust_build = "devalpha"
 no_pwd_login = False
 sys_update_check_server = 'http://info.flyos.us/latest_ver'
